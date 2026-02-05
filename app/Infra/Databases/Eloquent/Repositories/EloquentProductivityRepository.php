7fe06f5f5916893c3eabd4b2d8adcab9cb3d3f95
Gabriel N <gabrielnaldi.castro@gmail.com>
2026-02-05 17:35:08 -0300

    feat: implement eloquent repository for productivities

---
+++ app/Infra/Databases/Eloquent/Repositories/EloquentProductivityRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Infra\Databases\Eloquent\Repositories;
+
+use App\Domain\Entities\Productivity\Productivity;
+use App\Domain\Repositories\Productivity\ProductivityRepository;
+use App\Infra\Databases\Eloquent\Mappers\ProductivityMapper;
+use App\Models\ProductivityModel;
+
+class EloquentProductivityRepository implements ProductivityRepository {
+    public function save(Productivity $productivity): Productivity {
+        ProductivityModel::updateOrCreate([
+            'id' => $productivity->id(),
+            [
+                'product' => $productivity->product(),
+                'produced' => $productivity->produced(),
+                'defects' => $productivity->defects(),
+                'created_at' => $productivity->createdAt()->format('Y-m-d H:i:s'),
+            ]
+        ]);
+
+        return $productivity;
+    }
+
+    public function findById(string $id): ?Productivity {
+        $model = ProductivityModel::find($id);
+
+        if(!$model) return null;
+
+        return ProductivityMapper::toDomain($model);
+    }
+
+    /** @return Productivity[] */
+    public function list(int $page, int $limit): array {
+        $models = ProductivityModel::query()
+        ->orderBy('created_at', 'desc')
+        ->offset(($page - 1) * $limit)
+        ->limit($limit)
+        ->get();
+
+        $mappedModels = $models->map(fn ($model) => ProductivityMapper::toDomain($model))->toArray();
+
+        return $mappedModels;
+    }
+}
