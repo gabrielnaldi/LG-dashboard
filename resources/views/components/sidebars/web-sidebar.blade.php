40a6ba2566eb0ddf3ff6e2fab922337cef11a835
Gabriel N <gabrielnaldi.castro@gmail.com>
2026-02-06 15:55:38 -0300

    feat: create web sidebar

---
+++ resources/views/components/sidebars/web-sidebar.blade.php
@@ -0,0 +1,14 @@
+<div id="web-sidebar" class="hidden md:block">
+    <div class="w-64 h-full bg-sidebar shadow-lg">
+        <div class="w-full flex justify-center py-5 px-3 mb-4">
+            <h1 class="text-white font-bold text-2xl">LG Dashboard</h1>
+        </div>
+        <nav class="p-4">
+            <ul>
+                <li>
+                <a href="/productivities" class="block text-white p-2 w-full rounded-md font-medium hover:bg-white hover:font-bold hover-text-dark-blue">Produtividade</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</div>
