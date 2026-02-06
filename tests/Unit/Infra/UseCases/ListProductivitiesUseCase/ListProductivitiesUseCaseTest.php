311785c5289bea50b54d587921b930109ef3190e
Gabriel N <gabrielnaldi.castro@gmail.com>
2026-02-06 12:56:12 -0300

    fix: test on list use case

--- tests/Unit/Infra/UseCases/ListProductivitiesUseCase/ListProductivitiesUseCaseTest.php
+++ tests/Unit/Infra/UseCases/ListProductivitiesUseCase/ListProductivitiesUseCaseTest.php
@@ -3,6 +3,7 @@
 
 namespace Tests\Unit\Infra\UseCases\ListProductivitiesUseCase;
 
+use App\Application\UseCases\ListProductivitiesUseCase\ListProductivitiesOutput;
 use App\Application\UseCases\ListProductivitiesUseCase\ListProductivitiesUseCase;
 use App\Domain\Entities\Productivity\Productivity;
 use App\Domain\Repositories\Productivity\ProductivityRepository;
@@ -28,16 +29,16 @@ class ListProductivitiesUseCaseTest extends TestCase {
     public function test_it_should_list_with_pagination(): void {
         // First check
         $firstResult = $this->usecase->execute(1, 10);
+        $this->assertInstanceOf(ListProductivitiesOutput::class, $firstResult);
+        $this->assertIsArray($firstResult->items);
+        $this->assertCount(10, $firstResult->items);
+        $this->assertEquals('Produto 1', $firstResult->items[0]->product());
+        $this->assertEquals('Produto 10', $firstResult->items[9]->product());
 
-        $this->assertIsArray($firstResult);
-        $this->assertCount(10, $firstResult);
-        $this->assertEquals('Produto 1', $firstResult[0]->product());
-        $this->assertEquals('Produto 10', $firstResult[9]->product());
-
-        // Second page check
+        // // Second page check
         $secondResult = $this->usecase->execute(3, 5);
-        $this->assertCount(5, $secondResult);
-        $this->assertEquals('Produto 11', $secondResult[0]->product());
-        $this->assertEquals('Produto 15', $secondResult[4]->product());
+        $this->assertCount(5, $secondResult->items);
+        $this->assertEquals('Produto 11', $secondResult->items[0]->product());
+        $this->assertEquals('Produto 15', $secondResult->items[4]->product());
     }
 }
